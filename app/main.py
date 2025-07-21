626aaa6c631cd92bef9e8d80335fed8eaaa4ef62
Yurii Palahitskyi <brebston7@gmail.com>
2025-07-21 10:37:26 +0200

    Fixed issues

--- app/main.py
+++ app/main.py
@@ -15,8 +15,8 @@ def create_person_list(data_list: list) -> list:
 
     for data in data_list:
         person = Person.people[data["name"]]
-        if "wife" in data and data["wife"]:
+        if "wife" in data and data["wife"] is not None:
             person.wife = Person.people[data["wife"]]
-        if "husband" in data and data["husband"]:
+        if "husband" in data and data["husband"] is not None:
             person.husband = Person.people[data["husband"]]
     return persons
